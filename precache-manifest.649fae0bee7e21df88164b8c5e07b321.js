17bdb16526dae1fe8f3dceca34385842c1dcea8a
Dominik Swiatek <dominik.swiatek1995@gmail.com>
2020-03-25 12:31:44 +0100

    Updates

---
+++ precache-manifest.649fae0bee7e21df88164b8c5e07b321.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a32006206a362c07cfb92d09e5100a67",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "1759c07f53d330f32256",
+    "url": "/calculator/static/css/main.2646604b.chunk.css"
+  },
+  {
+    "revision": "4a89dbb1ff943036d770",
+    "url": "/calculator/static/js/2.f6f60222.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.f6f60222.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1759c07f53d330f32256",
+    "url": "/calculator/static/js/main.1f662fc6.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
